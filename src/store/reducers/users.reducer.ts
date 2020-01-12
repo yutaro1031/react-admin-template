f703f04bff8cdd98f1666f32e1b42b1bf16f3c99
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-01-12 12:50:41 +0900

    Use "typescript-fsa" in users

--- src/store/reducers/users.reducer.ts
+++ src/store/reducers/users.reducer.ts
@@ -1,5 +1,6 @@
 import { IUserState, IActionBase } from "../models/root.interface";
 import { ADD_ADMIN, REMOVE_ADMIN } from "../actions/users.action";
+import { IUser } from "../models/user.interface";
 
 const initialState: IUserState = {
     users: [
@@ -12,12 +13,13 @@ const initialState: IUserState = {
 };
 
 function userReducer(state: IUserState = initialState, action: IActionBase): IUserState {
+    const user: IUser = action.payload;
     switch (action.type) {
         case ADD_ADMIN: {
-            return { ...state, users: state.users.filter(x=>x.id !== action.user.id), admins: [...state.admins, action.user]};
+            return { ...state, users: state.users.filter(x=>x.id !== user.id), admins: [...state.admins, user]};
         }
         case REMOVE_ADMIN: {
-            return { ...state, admins: state.admins.filter(x=>x.id !== action.user.id), users: [...state.users, action.user]};
+            return { ...state, admins: state.admins.filter(x=>x.id !== user.id), users: [...state.users, user]};
         }
         default:
             return state;
