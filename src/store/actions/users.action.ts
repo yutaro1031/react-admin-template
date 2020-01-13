6d70a14496f2662d5ede3024424c9b7a4dd17cde
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-01-13 09:24:13 +0900

    Revert "Add "typescript-fsa" to users"
    This reverts commit 4c6da7ba1f56f149546626410f4345146d557977.

--- src/store/actions/users.action.ts
+++ src/store/actions/users.action.ts
@@ -3,20 +3,20 @@ import { IUser } from "../models/user.interface";
 
 const actionCreator = actionCreatorFactory();
 
-// export const ADD_ADMIN: string = "ADD_ADMIN";
-// export const REMOVE_ADMIN: string = "REMOVE_ADMIN";
+export const ADD_ADMIN: string = "ADD_ADMIN";
+export const REMOVE_ADMIN: string = "REMOVE_ADMIN";
 
 // export function addAdmin(user: IUser): IAddAdminActionType {
 //     return { type: ADD_ADMIN, user: user };
 // }
 
-export const addAdmin = actionCreator<IUser>("ADD_ADMIN");
+export const addAdmin = actionCreator<IUser>(ADD_ADMIN);
 
 // export function removeAdmin(user: IUser): IRemoveAdminActionType {
 //     return { type: REMOVE_ADMIN, user: user };
 // }
 
-export const removeAdmin = actionCreator<IUser>("REMOVE_ADMIN");
+export const removeAdmin = actionCreator<IUser>(REMOVE_ADMIN);
 
 
 // interface IAddAdminActionType { type: string, user: IUser };
--- src/store/reducers/users.reducer.ts
+++ src/store/reducers/users.reducer.ts
@@ -1,8 +1,5 @@
-import { reducerWithInitialState } from "typescript-fsa-reducers";
-import { addAdmin, removeAdmin } from "../actions/users.action";
-
 import { IUserState, IActionBase } from "../models/root.interface";
-// import { ADD_ADMIN, REMOVE_ADMIN } from "../actions/users.action";
+import { ADD_ADMIN, REMOVE_ADMIN } from "../actions/users.action";
 import { IUser } from "../models/user.interface";
 
 const initialState: IUserState = {
@@ -15,30 +12,18 @@ const initialState: IUserState = {
     ]
 };
 
-export const userReducer = reducerWithInitialState(initialState)
-    .case(addAdmin, (state, payload: IUser) => ({
-        ...state,
-        users: state.users.filter(x => x.id !== payload.id),
-        admins: [...state.admins, payload]
-    }))
-    .case(removeAdmin, (state, payload: IUser) => ({
-        ...state,
-        users: [...state.users, payload],
-        admins: state.admins.filter(x => x.id !== payload.id)
-    }))
-
-// function userReducer(state: IUserState = initialState, action: IActionBase): IUserState {
-//     const user: IUser = action.payload;
-//     switch (action.type) {
-//         case ADD_ADMIN: {
-//             return { ...state, users: state.users.filter(x=>x.id !== user.id), admins: [...state.admins, user]};
-//         }
-//         case REMOVE_ADMIN: {
-//             return { ...state, admins: state.admins.filter(x=>x.id !== user.id), users: [...state.users, user]};
-//         }
-//         default:
-//             return state;
-//     }
-// }
+function userReducer(state: IUserState = initialState, action: IActionBase): IUserState {
+    const user: IUser = action.payload;
+    switch (action.type) {
+        case ADD_ADMIN: {
+            return { ...state, users: state.users.filter(x=>x.id !== user.id), admins: [...state.admins, user]};
+        }
+        case REMOVE_ADMIN: {
+            return { ...state, admins: state.admins.filter(x=>x.id !== user.id), users: [...state.users, user]};
+        }
+        default:
+            return state;
+    }
+}
 
 export default userReducer;
